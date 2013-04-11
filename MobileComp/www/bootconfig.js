48a731cc6b9d754f7842fb65afbcfedd80fe9b49
Muthu <muthu@softsquare.biz>
2013-04-11 12:26:03 +0530

    First

--- MobileComp/www/bootconfig.js
+++ MobileComp/www/bootconfig.js
@@ -21,7 +21,7 @@ var oauthScopes = ["visualforce","api"];
 
 // The start page of the application.  This is the [pagePath] portion of
 // http://[host]/[pagePath].  Leave blank to use the local index.html page.
-var startPage = "";  // Used for local REST-based"index.html" PhoneGap app.
+var startPage = "/apex/MobilePage";  // Used for local REST-based"index.html" PhoneGap app.
 //var startPage = "apex/BasicVFPage"; //used for Visualforce-based apps
 
 
